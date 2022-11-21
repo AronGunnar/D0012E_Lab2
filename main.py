cd374edde17900bf3393b58603a765f77a57c2fe
AronGunnar <1oscar.boren@gmail.com>
2022-11-21 13:10:18 +0100

    Part 1 working

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 
 
 # =============== Part One ===============
-def incr_triple(lst):  # <---- Should be O(n)?
+def incr_triple(lst):  # Todo: Check that its O(n).
     temp = []
     for i, val in enumerate(lst):
         if len(temp) < 3:
@@ -22,24 +22,28 @@ def incr_triple(lst):  # <---- Should be O(n)?
     return sort(temp)
 
 
-def dac_triple(lst):  # WIP
-    sublsts = []
-    # for i, val in enumerate(lst):
-    #     sublsts.append([val])
+def dac_triple(lst):  # Todo: Check that its O(n).
+    if len(lst) == 3:
+        return sort(lst)
+    else:
+        mid = len(lst) // 2
+        lst_l = dac_triple(lst[mid:])
+        lst_r = dac_triple(lst[:mid])
 
-    mid = len(lst) // 2
+    lst_l = sort(lst_l)
+    lst_r = sort(lst_r)
+    for i, val_r in enumerate(lst_r):
+        for j, val_l in enumerate(lst_l):
+            if val_l < val_r:
+                lst_r[i] = val_l
+                lst_l[j] = val_r
+                val_r = val_l
 
-    if len(lst) >= 3:
-        lst = dac_triple(lst[0:mid])
-
-    if len(lst) > 1:
-        return lst[0] if lst[0] < lst[1] else lst[1]
-
-    return lst
+    return sort(lst_r)
 
 
-def sort(lst):  # Insertionsort, since data set contains 3 elements no fancier sort is needed.
-    for j, val in enumerate(lst):  # n-1
+def sort(lst):  # Insertionsort, since data set contains 3 elements no fancier sort is needed. (O(n^2), n = 3 => O(27))
+    for j, val in enumerate(lst):
         i = j - 1
         while i >= 0 and lst[i] > val:
             lst[i + 1] = lst[i]
@@ -53,10 +57,14 @@ def sort(lst):  # Insertionsort, since data set contains 3 elements no fancier s
 
 # =============== Testing/Printing ===============
 if __name__ == '__main__':
-    lst = [randrange(10) for i in range(10)]  # <---- Make elements is data set distinct.
-    # lst = [9, 8, 2, 7, 3, 8, 9, 7, 0, 6]  # For testing
+    k = 3
+    length = 3 * 2 ** (k - 1)  # Given in instructions, lenght of data set is always divisible by 3.
+    a = [randrange(10) for i in range(length)]  # <---- Todo: Make elements is data set distinct.
+    # a = [2, 6, 9, 3, 4, 7]  # For testing
+
+    # ---------- Testing ----------
 
     # ---------- Printing ----------
-    print(lst)
-    print("Smallest elements in set: ", incr_triple(lst))
-    # print("Smallest elements in set: ", dac_triple(lst))
+    print("\nData set: ", a)
+    # print("Smallest elements in set: ", incr_triple(a))
+    print("Smallest elements in set: ", dac_triple(a))
